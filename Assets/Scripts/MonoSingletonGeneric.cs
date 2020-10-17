a4102f4b68666627d4a35adc32163ae31c0b63d9
Viditv68 <viditrawatv68@gmail.com>
2020-10-17 11:50:31 +0530

    Singleton- Assignment 1

---
+++ Assets/Scripts/MonoSingletonGeneric.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MonoSingletonGeneric<T> : MonoBehaviour where T : MonoSingletonGeneric<T>
+{
+	private static T instance;
+	public static T Instance { get { return instance; } }
+
+	private void Awake()
+    {
+		if(instance == null)
+        {
+			instance = (T)this;
+			Debug.Log(instance.name);
+        }
+
+        else 
+		{
+			Destroy(this);
+		}
+    }
+	
+}
